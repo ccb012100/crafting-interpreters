6404ab449052b6327c017c9ab002c57522f2b495
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-04-21 15:32:45 -0500

    4.2.3 Location Information

--- cslox/Token.cs
+++ cslox/Token.cs
@@ -2,4 +2,18 @@ namespace cslox;
 
 public class Token
 {
+    public TokenType Type;
+    public string Lexeme;
+    public object Literal;
+    public int Line;
+
+    public Token(TokenType type, string lexeme, object literal, int line)
+    {
+        Type = type;
+        Lexeme = lexeme;
+        Literal = literal;
+        Line = line;
+    }
+
+    public override string ToString() => $"{Type} {Lexeme} {Literal}";
 }
