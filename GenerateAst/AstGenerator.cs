9e3a8aea7b99c1f78025e792a07b6b3addb3db07
Christopher Bocardo <ccb012100@users.noreply.github.com>
2022-05-25 14:26:03 -0500

    5.2 Implementing Syntax Trees
    Adds the GenerateAst project to generate the syntax classes.

---
+++ GenerateAst/AstGenerator.cs
@@ -0,0 +1,58 @@
+﻿internal static class AstGenerator
+{
+    public static void defineAst(string outputDir, string baseName, List<string> types)
+    {
+        string path = $"{outputDir}/{baseName}.cs";
+
+        using StreamWriter writer = File.CreateText(path);
+        writer.WriteLine("namespace cslox;");
+        writer.WriteLine();
+        writer.WriteLine($"internal abstract class {baseName}");
+        writer.WriteLine("{");
+
+        foreach (string type in types)
+        {
+            string[] typeSplit = type.Split(':');
+
+            string className = typeSplit[0].Trim();
+            string fields = typeSplit[1].Trim();
+
+            defineType(className, fields);
+        }
+
+        writer.WriteLine("}");
+        writer.Close();
+
+        void defineType(string className, string fieldList)
+        {
+            const string tab = "    ";
+
+            writer.WriteLine($"{tab}public class {className} : {baseName}");
+            writer.WriteLine($"{tab}{{");
+
+            // ctor
+            writer.WriteLine($"{tab}{tab}public {className} ({fieldList})");
+            writer.WriteLine($"{tab}{tab}{{");
+
+            string[] fields = fieldList.Split(", ");
+
+            // store params in fields
+            foreach (string field in fields)
+            {
+                string name = field.Split(" ")[1];
+                writer.WriteLine($"{tab}{tab}{tab}this.{name} = {name};");
+            }
+
+            writer.WriteLine($"{tab}{tab}}}");
+            writer.WriteLine();
+
+            // fields
+            foreach (string field in fields)
+            {
+                writer.WriteLine($"{tab}{tab}public {field};");
+            }
+
+            writer.WriteLine($"{tab}}}");
+        }
+    }
+}
---
+++ GenerateAst/Program.cs
@@ -0,0 +1,25 @@
+﻿// ReSharper disable once ClassNeverInstantiated.Global
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        if (args.Length is not 1)
+        {
+            Console.Error.WriteLine("Usage: generate_ast <output directory>");
+            Environment.Exit(64);
+        }
+
+        string outputDir = args[0];
+
+        // NOTE: using `oper` because `operator` is a keyword in C#
+        AstGenerator.defineAst(outputDir, "Expr",
+            new List<string>
+            {
+                "Binary   : Expr left, Token oper, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : object value",
+                "Unary    : Token oper, Expr right"
+            });
+    }
+}
