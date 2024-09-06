d94348fb234b6d0014fc6c254210ad4f269172c2
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-09-05 19:04:39 -0500

    add comment with Expression Grammar to Parser

--- cslox/cslox/Parser.cs
+++ cslox/cslox/Parser.cs
@@ -2,6 +2,31 @@
 
 using static TokenType;
 
+/*
+ *   --------------------------------------------------------------------------
+ *                         Expression Grammar
+ *   --------------------------------------------------------------------------
+ *     expression          →     equality ;
+ *     equality            →     comparison ( ( "!=" | "==" ) comparison )* ;
+ *     comparison          →     term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+ *     term                →     factor ( ( "-" | "+" ) factor )* ;
+ *     factor              →     unary ( ( "/" | "*" ) unary )* ;
+ *     unary               →     ( "!" | "-" ) unary
+ *                         |     primary ;
+ *     primary             →     NUMBER | STRING | "true" | "false" | "nil"
+ *                         |     "(" expression ")" ;
+ *   --------------------------------------------------------------------------
+ *
+ *   --------------------------------------------------------------------------
+ *     Grammar notation    |   Code representation
+ *   ----------------------|---------------------------------------------------
+ *     Terminal            |   Code to match and consume a token
+ *     Non-terminal        |   Call to that rule’s function
+ *     |                   |   if or switch statement
+ *     * or +              |   while or for loop
+ *     ?                   |   if statement
+ *   --------------------------------------------------------------------------
+ */
 internal class Parser
 {
     private readonly List<Token> _tokens;
