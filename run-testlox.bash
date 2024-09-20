6248dae88515f32a82f5d232bcac3630353c0edc
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-09-19 19:21:16 -0500

    fix: typo in rest-testlox.bash

--- run-testlox.bash
+++ run-testlox.bash
@@ -3,5 +3,5 @@ set -Eeou pipefail
 
 scriptdir=$(dirname -- "$(readlink -f -- "$0")")
 
-echo '> dotnet run --project' "$scriptdir/cslox/cslox/mv cslox.csproj" "$scriptdir/cslox/cslox/test-code/test.lox"
+echo '> dotnet run --project' "$scriptdir/cslox/cslox/cslox.csproj" "$scriptdir/cslox/cslox/test-code/test.lox"
 dotnet run --project "$scriptdir/cslox/cslox/cslox.csproj" "$scriptdir/cslox/cslox/test-code/test.lox"
