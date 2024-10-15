6dafb49fe363419b61a64583d72c0d7bec6bf5c4
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2024-10-15 06:48:45 -0500

    add clox.bash script for running clox

--- cslox.bash
+++ cslox.bash
@@ -3,5 +3,5 @@ set -Eeou pipefail
 
 scriptdir=$(dirname -- "$(readlink -f -- "$0")")
 
-echo '> dotnet run --project' "$scriptdir/cslox/cslox/mv cslox.csproj" "$@"
+echo '➤ dotnet run --project' "$scriptdir/cslox/cslox/mv cslox.csproj" "$@"
 dotnet run --project "$scriptdir/cslox/cslox/cslox.csproj" "$@"
